771c881bc866b3085a21dbc607ff1946e6177c62
penghaoze <2235710879@qq.com>
2019-03-25 21:27:09 +0800

    update

---
+++ src/net/BufferWriter.cpp
@@ -0,0 +1,134 @@
+﻿// PHZ
+// 2018-5-15
+
+#include "BufferWriter.h"
+#include "Socket.h"
+#include "SocketUtil.h"
+
+using namespace xop;
+
+void xop::writeInt32BE(char* p, int32_t value)
+{
+    p[0] = value >> 24;
+	p[1] = value >> 16;
+	p[2] = value >> 8;
+	p[3] = value & 0xff;
+}
+
+void xop::writeInt32LE(char* p, int32_t value)
+{
+    p[0] = value & 0xff;
+	p[1] = value >> 8;
+	p[2] = value >> 16;
+	p[3] = value >> 24;
+}
+
+void xop::writeInt24BE(char* p, int32_t value)
+{
+	p[0] = value >> 16;
+	p[1] = value >> 8;
+	p[2] = value & 0xff;
+}
+
+void xop::writeInt24LE(char* p, int32_t value)
+{
+    p[0] = value & 0xff;
+	p[1] = value >> 8;
+	p[2] = value >> 16;
+}
+
+void xop::writeInt16BE(char* p, int16_t value)
+{
+    p[0] = value >> 8;
+	p[1] = value & 0xff;
+}
+
+void xop::writeInt16LE(char* p, int16_t value)
+{
+    p[0] = value & 0xff;
+	p[1] = value >> 8;
+}
+
+BufferWriter::BufferWriter(int capacity) 
+    : _maxQueueLength(capacity)
+	, _buffer(new std::queue<Packet>)
+{
+	
+}	
+
+bool BufferWriter::append(std::shared_ptr<char> data, uint32_t size, uint32_t index)
+{
+    if(size <= index)
+        return false;
+
+    if((int)_buffer->size() >= _maxQueueLength)
+        return false;		
+
+    Packet pkt = {data, size, index};
+    _buffer->emplace(std::move(pkt));
+
+    return true;
+}
+
+bool BufferWriter::append(const char* data, uint32_t size, uint32_t index)
+{
+    if(size <= index)
+        return false;
+
+    if((int)_buffer->size() >= _maxQueueLength)
+        return false;		
+
+    Packet pkt;
+    pkt.data.reset(new char[size+512]);
+    memcpy(pkt.data.get(), data, size);
+    pkt.size = size;
+    pkt.writeIndex = index;
+
+    _buffer->emplace(std::move(pkt));
+
+    return true;
+}
+
+int BufferWriter::send(int sockfd, int timeout)
+{		
+    if(timeout > 0)
+        SocketUtil::setBlock(sockfd, timeout); // 超时返回-1
+
+	int ret = 0;
+	int count = 1;
+	do
+	{
+		if (_buffer->empty())
+			return 0;
+
+		count -= 1;
+		Packet &pkt = _buffer->front();
+		ret = ::send(sockfd, pkt.data.get() + pkt.writeIndex, pkt.size - pkt.writeIndex, 0);
+		if (ret > 0)
+		{
+			pkt.writeIndex += ret;
+			if (pkt.size == pkt.writeIndex)
+			{
+				count += 1;
+				_buffer->pop();
+			}
+		}
+		else if (ret < 0)
+		{
+#if defined(__linux) || defined(__linux__)
+			if (errno == EINTR || errno == EAGAIN)
+#elif defined(WIN32) || defined(_WIN32)
+			int error = WSAGetLastError();
+			if (error == WSAEWOULDBLOCK || error == WSAEINPROGRESS || error == 0)
+#endif
+				ret = 0;
+		}
+	} while (count>0);
+
+    if(timeout > 0)
+        SocketUtil::setNonBlock(sockfd);
+
+    return ret;
+}
+
+
---
+++ src/net/BufferWriter.h
@@ -0,0 +1,58 @@
+﻿// PHZ
+// 2018-5-15
+
+#ifndef XOP_BUFFER_WRITER_H
+#define XOP_BUFFER_WRITER_H
+
+#include <cstdint>
+#include <memory>
+#include <queue>
+#include <string>
+
+namespace xop
+{
+
+void writeInt32BE(char* p, int32_t value);
+void writeInt32LE(char* p, int32_t value);
+void writeInt24BE(char* p, int32_t value);
+void writeInt24LE(char* p, int32_t value);
+void writeInt16BE(char* p, int16_t value);
+void writeInt16LE(char* p, int16_t value);
+	
+class BufferWriter
+{
+public:
+    BufferWriter(int capacity=kMaxQueueLength);
+    ~BufferWriter() {}
+
+    bool append(std::shared_ptr<char> data, uint32_t size, uint32_t index=0);
+    bool append(const char* data, uint32_t size, uint32_t index=0);
+    int send(int sockfd, int timeout=0); // timeout: ms
+
+    bool isEmpty() const 
+    { return _buffer->empty(); }
+
+    bool isFull() const 
+    { return ((int)_buffer->size()>=_maxQueueLength?true:false); }
+
+    uint32_t size() const 
+    { return _buffer->size(); }
+	
+private:
+    typedef struct 
+    {
+        std::shared_ptr<char> data;
+        uint32_t size;
+        uint32_t writeIndex;
+    } Packet;
+
+    std::shared_ptr<std::queue<Packet>> _buffer;  		
+    int _maxQueueLength = 0;
+	 
+    static const int kMaxQueueLength = 30;
+};
+
+}
+
+#endif
+
